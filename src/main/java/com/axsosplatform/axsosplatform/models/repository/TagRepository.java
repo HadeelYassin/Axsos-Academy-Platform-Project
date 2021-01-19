cafb6574ea5402a63f1d717506d673dad1c46cce
Ahmad Salah <ahmadmahmoudsalah@gmail.com>
2021-01-19 19:09:24 +0200

    test conflict

--- src/main/java/com/axsosplatform/axsosplatform/models/repository/TagRepository.java
+++ src/main/java/com/axsosplatform/axsosplatform/models/repository/TagRepository.java
@@ -4,7 +4,10 @@ import com.axsosplatform.axsosplatform.models.Tag;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface TagRepository extends CrudRepository<Tag,Long> {
+    List<Tag> findTagBy(Tag tag);
 }
