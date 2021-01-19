99ab0dd081c21540a2916bf1dad0d186e069bb44
Ahmad Salah <ahmadmahmoudsalah@gmail.com>
2021-01-19 19:39:21 +0200

    move repository to the correct place

--- src/main/java/com/axsosplatform/axsosplatform/repository/UserRoleRepository.java
+++ src/main/java/com/axsosplatform/axsosplatform/repository/UserRoleRepository.java
@@ -1,4 +1,4 @@
-package com.axsosplatform.axsosplatform.models.repository;
+package com.axsosplatform.axsosplatform.repository;
 
 import com.axsosplatform.axsosplatform.models.User;
 import org.springframework.data.repository.CrudRepository;
