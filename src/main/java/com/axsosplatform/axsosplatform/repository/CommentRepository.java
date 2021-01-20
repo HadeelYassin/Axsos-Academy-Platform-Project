14c000588b29b66d0469d1b46af71eb8807dfe50
Ahmad Salah <ahmadmahmoudsalah@gmail.com>
2021-01-20 14:38:00 +0200

    user

--- src/main/java/com/axsosplatform/axsosplatform/repository/CommentRepository.java
+++ src/main/java/com/axsosplatform/axsosplatform/repository/CommentRepository.java
@@ -3,9 +3,6 @@ package com.axsosplatform.axsosplatform.repository;
 import com.axsosplatform.axsosplatform.models.Comment;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
 @Repository
 public interface CommentRepository extends CrudRepository<Comment,Long> {
 
